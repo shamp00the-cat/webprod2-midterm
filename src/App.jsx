87fb7482b7efd27511af605a97e1b86ee01c891a
Casey Roosma <casey.roosma75@myhunter.cuny.edu>
2023-10-15 22:55:11 -0400

    fixed searchgit statusgit status

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 //import ListGroup from "./components/ListGroup";
-import "bootstrap/dist/css/bootstrap.css";
-import { Container, InputGroup, FormControl, Card, Row, Button } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useState, useEffect } from "react";
+import {
+  Container,
+  InputGroup,
+  FormControl,
+  Row,
+  Card,
+  Button,
+} from "react-bootstrap";
 import { ClientID, ClientSecret, RedirectURI } from "./components/ListGroup";
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
   useEffect(() => {
     var authParameters = {
       method: "POST",
@@ -21,9 +31,46 @@ function App() {
 
     fetch("https://accounts.spotify.com/api/token", authParameters)
       .then((result) => result.json())
-      .then((data) => console.log(data));
+      .then((data) => setAccessToken(data.access_token));
   }, []);
 
+  async function search() {
+    console.log("Search for " + searchInput);
+
+    //get request using search to get artist ID
+    var searchParameters = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+    var artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    console.log("Artist ID is " + artistID);
+    //get request with artist ID grab all the albums from artist
+    var returnedAlbums = await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums" +
+        "?include_groups=album&market=US&limit=50",
+      searchParameters
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setAlbums(data.items);
+      });
+    //display albums to user
+  }
+  console.log(albums);
   return (
     <div className="App">
       <Container>
@@ -33,27 +80,27 @@ function App() {
             type="input"
             onKeyDown={(event) => {
               if (event.key == "Enter") {
-                console.log("pressed enter");
+                search();
               }
             }}
             onChange={(event) => setSearchInput(event.target.value)}
           />
-          <Button
-            onClick={() => {
-              console.log("Clicked Button");
-            }}
-          >
-            Search
-          </Button>
+          <Button onClick={search}>Search</Button>
         </InputGroup>
       </Container>
       <Container>
         <Row className="mx-2 row row-cols-4"></Row>
-        <Card>
-          <Card.Body>
-            <Card.Title>Album Name</Card.Title>
-          </Card.Body>
-        </Card>
+        {albums.map((album, i) => {
+          console.log(album);
+          return (
+            <Card>
+              <Card.Img src={album.images[0].url} />
+              <Card.Body>
+                <Card.Title>{album.name}</Card.Title>
+              </Card.Body>
+            </Card>
+          );
+        })}
       </Container>
     </div>
   );
